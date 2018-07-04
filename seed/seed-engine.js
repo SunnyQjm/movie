1014f6dab4319457f8722392f337b3b9062baeb2
MingJ <qjm253@gmail>
2018-07-04 21:06:12 +0800

    修改seed engine

--- seed/seed-engine.js
+++ seed/seed-engine.js
@@ -8,18 +8,18 @@ let getClient = thunky(cb => {
 function seedFiles(path, cb){
     getClient(client => {
         client.seed(path, {
-            // announce: [
-            //     'http://210.30.100.171:8000/announce',
-            //     'udp://0.0.0.0:8000',
-            //     'udp://210.30.100.171:8000',
-            //     'ws://210.30.100.171:8000',
-            // ]
             announce: [
-                'http://localhost:8000/announce',
+                'http://210.30.100.171:8000/announce',
                 'udp://0.0.0.0:8000',
-                'udp://localhost.171:8000',
-                'ws://localhost.171:8000',
+                'udp://210.30.100.171:8000',
+                'ws://210.30.100.171:8000',
             ]
+            // announce: [
+            //     'http://localhost:8000/announce',
+            //     'udp://0.0.0.0:8000',
+            //     'udp://localhost.171:8000',
+            //     'ws://localhost.171:8000',
+            // ]
         }, torrent => {
             console.log(torrent.magnetURI);
             if(cb)
